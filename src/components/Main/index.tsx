5ab632f628cf3cb1b96954cc079c83f9d45837ce
Eduardo Lopes <eduardo.santos@hexalab.com.br>
2022-11-20 16:13:51 -0300

    feat: Header component

--- src/components/Main/index.tsx
+++ src/components/Main/index.tsx
@@ -1,10 +1,10 @@
-import { Text } from "../Text";
+import { Header } from "../Header";
 import { Container } from "./styles";
 
 export function Main(){
 	return (
 		<Container>
-			<Text>Main renderizou</Text>
+			<Header />
 		</Container>
 	);
 }
